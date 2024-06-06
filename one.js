5f68e283227c0737c39ab49be17a63624eb6833a
sukeshkumar2 <dosapatisukesh@gmail.com>
2024-06-06 15:44:07 -0700

    committing index.html and one.js files

---
+++ one.js
@@ -0,0 +1,25 @@
+// function getPrompt()
+// {
+//     result = prompt('Enter your name');
+//     return result;
+// }
+
+// result = getPrompt();
+// let confirmation = confirm(`Is your name ${result} please confirm with yes or no`)
+
+// if(confirmation)
+//     {
+//         alert(`Hello ${result}`)
+//     }
+//     else{
+//         alert('Hello Alien')
+//     }
+
+
+// console.log('value from prompt '+ '2'*'3');
+
+let num ='    '
+alert(typeof num)
+
+num = Number(num)
+alert(`${num}`)
